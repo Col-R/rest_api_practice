310b8c3b85f1c7dbc82efc3f923af8381a3339b3
Col-R <2colewestin@gmail.com>
2022-06-03 15:03:25 -0500

    added some dependencies and data for display

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Just some practice on rest APIs using express and node - 6/3/22
+
+install json dependencies, npm start to run server
--- index.js
+++ index.js
@@ -6,4 +6,11 @@ const { v4: uuid } = require("uuid");
 
 const app = express ();
 
+app.get("/outfit", (req, res) => {
+    const tops = ["T-Shirt", "Sweater", "Button-Up"];
+    const bottoms = ["Shorts", "Pants", "Jeans"];
+    const shoes = ["Sneakers", "Sandals", "Boots"];
+    res.send("Homepage working")
+});
+
 app.listen(3000, () => console.log("Server is up and running"))
